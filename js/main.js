12a1a167c7e489033959ab9ce88485bfe870621d
rajithabhanuka <rajithabhanuka1@gmail.com>
2021-07-18 12:54:37 +0530

    second commit

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Rajitha Bhanuka", "Software Developer", "Freelancer"],
       typeSpeed: 100,
       loop: true,
     });
